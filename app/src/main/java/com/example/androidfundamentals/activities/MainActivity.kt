751e961b6f8b2109301c733c34ebea8f47e1f85c
Pedro Motta <pmotta@thoughtworks.com>
2019-06-30 00:56:24 -0300

    Implements bottom navigation menu

--- app/src/main/java/com/example/androidfundamentals/activities/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/activities/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.androidfundamentals.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidfundamentals.R
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemReselectedListener,
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener(this)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemReselectedListener(this)
+
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainer, MainFragment())
         fragmentTransaction.commit()
+    }
+
+    override fun onNavigationItemReselected(p0: MenuItem) {
+        Log.d("EventReselect", p0.itemId.toString())
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        Log.d("EventSelect", p0.toString())
+        return true
     }
 }
